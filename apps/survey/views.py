b29e257420e214c69ef23b8cc5e73864e668ed26
rabin001 <rabin.gora@gmail.com>
2019-02-26 23:22:54 -0800

    first commit

---
+++ apps/survey/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# front page route
+def index(request):
+    # Initial session key setup
+    if "error_message" not in request.session:
+        request.session["error_message"] = ""
+        request.session["submit_count"] = 0
+
+    context = {
+            "error_message" : request.session["error_message"]
+    }
+
+    return render(request, "survey/index.html", context)
+
+# form process route
+def process(request):
+    # Case : raise error if name is not entered
+    if len(request.POST["name"]) < 1:
+        request.session["error_message"] = "Enter your name! (Not empty)"
+        return redirect("/")
+    
+    # Case : passing the form validation
+    else:
+        request.session["error_message"] = ""
+        request.session["name"] = request.POST["name"]
+        request.session["location"] = request.POST["location"]
+        request.session["favorite_language"] = request.POST["favorite_language"]
+        request.session["comment"] = request.POST["comment"]
+        request.session["submit_count"] += 1
+        return redirect("/result")
+
+# result render route
+def result(request):
+    context = {
+        "name" : request.session["name"],
+        "location" : request.session["location"],
+        "favorite_language" : request.session["favorite_language"],
+        "comment" : request.session["comment"],
+        "submit_count" : request.session["submit_count"]
+    }
+    return render(request, "survey/result.html", context)
+
+# session reset route
+def reset(request):
+    for key in request.session:
+        del request.session[key]
+
+    return redirect("/")
